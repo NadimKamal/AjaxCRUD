7b8b78634f60c614eef8f87c66a3c44899fdaa24
nadimkamal <nadimkamal.b@gmail.com>
2024-07-28 02:27:59 +0600

    Vendor Published

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,40 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Themegenie\Designer\Http\Controllers\StructureController;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $decoration= [
+        'class' => [
+            'formfooter'       => 'col-md-12 text-right',
+            'elementwrapper'   => 'col-md-4',
+            'elementcontainer' => 'mb-0'
+        ]
+    ];
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if(is_null(session('config')))
+        {
+            // (new StructureController)->structureComponentTree();
+        }
+        View::share('decoration', $this->decoration);
     }
 }
